4ce6ec8f595ca8a844a745029ae16872d0120ec9
Ashik Muhammed <ashikmuhammed@Ashiks-MacBook-Pro.local>
2025-03-13 12:06:18 +0530

    view engine set up

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const app = express()
 const env = require('dotenv').config()
-const ConnectDB = require('./config/db')
+const path = require('path')
 const connectDB = require('./config/db')
+const userRouter = require('./routes/userRouter')
 
 connectDB()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.set('view engine','ejs')
+app.set('views',[path.join(__dirname,'views/user'),path.join(__dirname,'views/admin')])
+app.use(express.static(path.join(__dirname,'public')))
+
+
+app.use('/',userRouter)
 
 app.listen(process.env.PORT,()=>{
   console.log('Server is running..')
---
+++ controllers/user/userController.js
@@ -0,0 +1,23 @@
+
+const loadHome = async (req,res)=>{
+  try {
+    return res.render('home')
+  } catch (error) {
+    console.log('Home page not found');
+    res.status(500).send('Server error')
+
+  }
+}
+
+const pageNotFound = async(req,res)=>{
+  try {
+     res.render('page-404')
+  } catch (error) {
+    res.redirect('/pageNotFound')
+  }
+}
+
+module.exports = {
+  loadHome,
+  pageNotFound,
+}
